d2879c3dafa7656f0a824b90ff796129a066dbe1
Orel-Viktor <OrelViktor93@gmail.com>
2022-08-31 16:15:39 +0300

    homework1

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+let age = 29;
+console.log('возвраст', typeof age);
+
+let userName ="Vitya";
+console.log ('имя', typeof userName);
+
+let bigNum = 4n;
+console.log('огромное число', typeof bigNum);
+ 
+let trueVal = 5>1;
+console.log( typeof trueVal);  
+console.log( 'истина', trueVal);
+let falseVal = 5<1;
+console.log( typeof falseVal);
+console.log( 'ложь', falseVal);
+
+let city = null;
+console.log( city); 
+console.log(typeof city); 
+
+let telephone;
+console.log(telephone);
+console.log(typeof telephone);
+
+let matrix =['1','2','3'];
+console.log (typeof matrix);
+console.log(matrix);
+
+let matrix2 ={planet:'Earht', system:'Sun'};
+console.log(typeof matrix2);
+console.log(matrix2);
+
+let spriteSym = Symbol('GG')
+console.log(typeof spriteSym)
+
+function calculate (a,b,c){
+  let value = a+b;
+  let values = value/c;
+  console.log(values)
+  console.log(typeof calculate)
+}
+calculate(5,1,2);
